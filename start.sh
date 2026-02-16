e817fe8d2cd5b23c5ab2cd3813f7b788a34aa0f2
Ferdinand Oeinck <fo@bigroses.net>
2026-02-16 17:26:20 +0100

    dotnet watch doens't work

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 
 cd /volume1/homes/fo/projects/dotnet/Solver
-export Microsoft_CodeAnalysis_EditAndContinue_LogDir=./../logs
-export DOTNET_ROOT=/var/services/homes/fo/projects/dotnet/.dotnet
-../.dotnet/dotnet watch --verbose run -c Release
+# export Microsoft_CodeAnalysis_EditAndContinue_LogDir=./../logs
+# export DOTNET_ROOT=/var/services/homes/fo/projects/dotnet/.dotnet
+../.dotnet/dotnet run -c Release
